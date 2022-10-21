eaa4bc74be6983930298b00303911d20dbbdd0a6
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2022-10-21 17:19:35 -0300

    requisito 2 feito

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <h1>TESTE</h1>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
